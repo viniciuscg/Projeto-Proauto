359309772b8c14184ec7150d595c50bf17d15a55
Vinicius <viniciuscgoncalves@hotmail.com>
2022-09-05 22:59:18 -0300

    ultimas alterações

--- backend/app/config/db.config.js
+++
@@ -1,6 +0,0 @@
-export const dbconfig = {
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    db: process.env.DB_DATABASE,
-}
--- backend/server.js
+++ backend/server.js
@@ -8,8 +8,6 @@ app.use(express.json())
 app.use(cors())
 app.use(associateRoutes)
 
-console.log(process.env.baldo)
-
 app.listen(3333, () => {
     console.log("Server running at 3333")
 })
